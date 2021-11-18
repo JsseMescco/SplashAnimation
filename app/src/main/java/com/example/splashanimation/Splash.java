122fd421b2a63edc362a31045fba9407e1f0d649
JsseMescco <jossemichaelmesccojorge@gmail.com>
2021-11-18 17:40:07 -0500

    Initial commit

---
+++ app/src/main/java/com/example/splashanimation/Splash.java
@@ -0,0 +1,42 @@
+package com.example.splashanimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Splash extends AppCompatActivity {
+    private static final long SPLASH_SCREEN_DISPLAY = 3000;
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView logo, Slogan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.botton_animation);
+
+        image = findViewById(R.id.imageView);
+        logo = findViewById(R.id.textView);
+        Slogan = findViewById(R.id.tv1);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        Slogan.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent( Splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN_DISPLAY);
+    }}
